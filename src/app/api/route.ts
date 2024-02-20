bdc01999009e456e6e7d9d5cac741a3d11cc5f90
Eric Madar <edmadar@gmail.com>
2024-02-20 01:42:47 -0800

    Cleaned up unnecessary files

--- src/app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import 'server-only'
-import { type NextRequest } from 'next/server'
-import mockServer from '../lib/mock-server';
-
-export async function GET(request: NextRequest) {
-    const data = mockServer();
-    console.log(`hit the request! data: ${JSON.stringify(data)}`);
-    return Response.json({ data });
-}
--- src/app/time/home-page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-interface Props {
-    time: string;
-}
-
-export default function HomePage({ time }: Props) {
-    return <div>The current time is: {time}</div>;
-}
